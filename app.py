2bde1d6dda23e6a72b0086a78c0813842a9cd00c
Asaf Bar-Lev <abarlev@gmail.com>
2019-01-09 15:01:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import pika
 import datetime
+import requests
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.check-sites.svc.cluster.local'))
 channel = connection.channel()
@@ -12,10 +13,15 @@ channel.queue_declare(queue='log')
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
     val = str(body).split(',')
-    log_message = "Processed {0!s} at {0!s}".format(val[0], str(datetime.datetime))
-    #print(val)
+    a = datetime.datetime.now()
+    r = requests.get(body)
+    b = datetime.datetime.now()
+    c = datetime.datetime.strptime(b, "%Y-%m-%dT%H:%M:%SZ") - datetime.datetime.strptime(a, "%Y-%m-%dT%H:%M:%SZ")
+    
+    log_message = "{0!s} {0!s} http status code: {0!s} took {{0!s}".format(str(datetime.datetime), val[0], r.status_code, c)
+    print(log_message)
     channel.basic_publish(exchange='', routing_key='hello', body=body)
-    channel.basic_publish(exchange='', routing_key='log', body=log_message)
+    #channel.basic_publish(exchange='', routing_key='log', body=log_message)
     print(" [x] Sent '%r'" % val)
 
 channel.basic_consume(callback,
