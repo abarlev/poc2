3bb5cc71eab3bc3fa18e4f81a32234cc40773b64
Asaf Bar-Lev <abarlev@gmail.com>
2019-01-10 08:36:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def callback(ch, method, properties, body):
     start = body_text.find('<title>') + 7
     end = body_text.find('</title>')
     print("start: {0}, end: {1}".format(start, end)
-    title = body_text[start : end]
+    title = 'title' #body_text[start : end]
 
     log_message = "{0} {1} http status code: {2} took {3} seconds. Title found: '{4}'".format(str(datetime.datetime.now()), 
                                                                           body, 
