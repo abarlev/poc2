b66d5f43a40ac62c606342a4989ea16a2f7aa771
Asaf Bar-Lev <abarlev@gmail.com>
2019-01-13 17:54:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,17 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq.ch
 channel = connection.channel()
 
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='sites')
 channel.queue_declare(queue='log')
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    data = json.loads(body)
+    print(" [x] Received %r" % data)
+    
     timeout = False
-    val = str(body).split(',')
     a = datetime.datetime.now()
     try:
-        r = requests.get(body)
+        r = requests.get(data['Site'])
     except Exception as e:
         timeout = True
         log_message = "{0} Exception: {1} URL: {2}".format(str(datetime.datetime.now()), str(e), body)
@@ -26,17 +27,23 @@ def callback(ch, method, properties, body):
         body_text = r.text
         start = body_text.find('<title>') + 7
         end = body_text.find('</title>')
-        #print("start: {0}, end: {1}".format(start, end))
+        is_title = True
         if start > 7:
             title = body_text[start : end]
+            if title == data[Title]:
+                title_match = True
+            else:
+                title_match = False
         else:
             title = 'no title found'
 
-        log_message = "{0} {1} http status code: {2} took {3} seconds. Title found: '{4}'".format(str(datetime.datetime.now()), 
+        log_message = "{0} {1} http status code: {2} took {3} seconds. Title match: {4}".format(str(datetime.datetime.now()), 
                                                                               body, 
                                                                               r.status_code,
                                                                               delta.total_seconds(),
-                                                                              title)
+                                                                              title_match)
+        if not title_match:
+            log_message = "{0}, expected: {1}, found: {2}".format(log_message, title, data[Title])
     print(log_message)
     # Not refilling queue yet
     #channel.basic_publish(exchange='', routing_key='hello', body=body)
